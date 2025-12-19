a230e7a3c0dffe3c7c1586827d047cbed4c26c1d
Gérald Barré <git@meziantou.net>
2025-12-18 20:39:39 -0500

    Format xml doc

--- Meziantou.MusicApp.Server/Models/CoverArtData.cs
+++ Meziantou.MusicApp.Server/Models/CoverArtData.cs
@@ -2,9 +2,7 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace Meziantou.MusicApp.Server.Models;
 
-/// <summary>
-/// Represents the data and metadata for a cover art image.
-/// </summary>
+/// <summary>Represents the data and metadata for a cover art image.</summary>
 public sealed class CoverArtData
 {
     [SuppressMessage("Performance", "CA1819:Properties should not return arrays", Justification = "This is a data transfer object representing binary image data")]
--- Meziantou.MusicApp.Server/Services/ImageCacheHelper.cs
+++ Meziantou.MusicApp.Server/Services/ImageCacheHelper.cs
@@ -2,14 +2,10 @@ using Microsoft.AspNetCore.Http;
 
 namespace Meziantou.MusicApp.Server.Services;
 
-/// <summary>
-/// Helper class providing common functionality for serving cached images
-/// </summary>
+/// <summary>Helper class providing common functionality for serving cached images</summary>
 public static class ImageCacheHelper
 {
-    /// <summary>
-    /// Sets HTTP cache headers for image responses based on file modification time
-    /// </summary>
+    /// <summary>Sets HTTP cache headers for image responses based on file modification time</summary>
     /// <param name="response">The HTTP response object</param>
     /// <param name="lastModified">The last modified date of the image file</param>
     public static void SetImageCacheHeaders(HttpResponse response, DateTimeOffset lastModified)
@@ -18,9 +14,7 @@ public static class ImageCacheHelper
         response.Headers.CacheControl = "public, max-age=2592000"; // 30 days
     }
 
-    /// <summary>
-    /// Checks if the client's cached version is still valid based on If-Modified-Since header
-    /// </summary>
+    /// <summary>Checks if the client's cached version is still valid based on If-Modified-Since header</summary>
     /// <param name="request">The HTTP request object</param>
     /// <param name="lastModified">The last modified date of the resource</param>
     /// <returns>True if the resource has not been modified since the client's cached version</returns>
