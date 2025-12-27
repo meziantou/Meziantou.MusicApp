3ddac7c12da6eedef15e36eac330352213263d70
Gérald Barré <meziantou@users.noreply.github.com>
2025-12-27 13:40:46 -0500

    Increment cache version to 2 in SerializableMusicCatalog

--- Meziantou.MusicApp.Server/Models/SerializableMusicCatalog.cs
+++ Meziantou.MusicApp.Server/Models/SerializableMusicCatalog.cs
@@ -6,10 +6,11 @@ internal sealed class SerializableMusicCatalog
     /// Current version of the cache format. Increment this when adding new properties
     /// or changing the structure to force a rescan of the library.
     /// </summary>
-    public const int CacheVersion = 1;
+    public const int CacheVersion = 2;
 
     public int Version { get; set; } = CacheVersion;
     public List<SerializableSong> Songs { get; set; } = [];
     public List<SerializablePlaylist> Playlist { get; set; } = [];
     public List<SerializableMissingPlaylistItem> MissingPlaylistItems { get; set; } = [];
 }
+
